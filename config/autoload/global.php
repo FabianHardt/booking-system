db4713f39dc9247966a3cc7f99f6fa37882b1e8e
Fabian Hardt <fabian.hardt@opitz-consulting.com>
2021-05-21 15:29:30 +0200

    quick button for manual activation

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,14 +12,14 @@ return array(
         'charset' => 'UTF8',
     ),
     'cookie_config' => array(
-        'cookie_name_prefix' => 'ep3-bs',
+        'cookie_name_prefix' => 'tcw-bs',
     ),
     'redirect_config' => array(
-        'cookie_name' => 'ep3-bs-origin',
+        'cookie_name' => 'tcw-bs-origin',
         'default_origin' => 'frontend',
     ),
     'session_config' => array(
-        'name' => 'ep3-bs-session',
+        'name' => 'tcw-bs-session',
         'save_path' => getcwd() . '/data/session/',
         'use_cookies' => true,
         'use_only_cookies' => true,
